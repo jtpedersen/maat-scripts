fc97f79fceab4b75b93f3e20efa7b04a31513bab
pedersen <git@rotand.dk>
2019-10-22 23:40:48 +0200

    add the missing break

--- miner/git_it.py
+++ miner/git_it.py
@@ -8,6 +8,8 @@ def map_log(fn, flags, repo):
     proc = subprocess.Popen(cmd,stdout=subprocess.PIPE, cwd=repo)
     lines = 0
     for line in iter(proc.stdout.readline,''):
+        if not line:
+            break
         fn(line.decode().strip())
         lines += 1
-    print(f"Parsed  {lines} lines of log data")
+    print(f"Mapped  {lines} lines of log data")
